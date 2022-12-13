882f105f820a069490bf8eb91ce95032a6be7010
ndlai <48231452+ndlai@users.noreply.github.com>
2022-12-13 19:00:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCha要要春rmXX')
+    print_hi('PyCha要要大有有有春rmXX')
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
